a1c8092a74a157df92046cceb7004b8af0311dff
Thor77 <thor77@thor77.org>
2016-10-03 21:53:59 +0200

    Use /search/issues instead of /issues
    * doesn't need repo-permission anymore
    * needs two more requests to fetch all the details from before
    * add headers(token) -> helper-function to generate headers with auth-token
    * fetch_login(token) -> fetch login-name
    * fetch_pull_requests(token, username) -> fetch pull requests,
    returns raw list of pull-request objects as returned by search

--- main.py
+++ main.py
@@ -14,10 +14,54 @@ client_secret = app.config.get('GITHUB_CLIENT_SECRET')
 api_base = 'https://api.github.com'
 
 auth_url = 'https://github.com/login/oauth/authorize'\
-    '?client_id={}&scope=public_repo'.format(client_id)
+    '?client_id={}'.format(client_id)
+
+time_range = '2016-10-01T00:00:01Z..2016-10-31T23:59:59'
+search_query = 'type:pr+created:' + time_range + '+author:{}'
 
 PullRequest = namedtuple('PullRequest', [
     'url', 'title', 'repo_url', 'repo_name', 'repo_owner'])
+
+
+def headers(token):
+    '''
+    Build authentication-headers with `token`
+    '''
+    return {
+        'Authorization': 'token {}'.format(token),
+        'User-Agent': 'HacktoberfestProgress/Thor77'
+    }
+
+
+def fetch_login(token):
+    '''
+    Fetch login-name for user authenticated by `token`
+    '''
+    r = requests.get(api_base + '/user', headers=headers(token))
+    if r.status_code != 200:
+        return None
+    return r.json()['login']
+
+
+def fetch_pull_requests(token, username):
+    '''
+    Fetch pull requests for `username`, authenticated by `token`
+    '''
+    # requests would urlencode the query so we fill it with a placeholder
+    params = {
+        'sort': 'created',
+        'q': 'QUERY'
+    }
+    req = requests.Request('GET', api_base + '/search/issues', params=params,
+                           headers=headers(token))
+    prepared = req.prepare()
+    # replace placeholder with actual query
+    prepared.url = prepared.url.replace('QUERY', search_query.format(username))
+    # send request
+    r = requests.Session().send(prepared)
+    if r.status_code != 200:
+        return []
+    return r.json()['items']
 
 
 @app.route('/')
@@ -60,30 +104,30 @@ def progress():
     if 'access_token' not in session:
         return redirect('/')
     access_token = session.get('access_token')
-    headers = {
-        'Authorization': 'token {}'.format(access_token)
-    }
-    r = requests.get(api_base + '/user/issues', headers=headers, params={
-        'filter': 'created',
-        'state': 'all',
-        'since': '2016-10-01T00:00:01Z'
-    })
-    if r.status_code != 200:
-        return render_template('error.jinja2', response=r)
-    r = r.json()
-    # check for errors
+    # obtain username
+    username = fetch_login(access_token)
+    raw_pull_requests = fetch_pull_requests(access_token, username)
+
     pull_requests = []
-    for issue_or_pr in r:
-        if 'pull_request' in issue_or_pr:
-            pr = issue_or_pr
-            pull_requests.append(
-                PullRequest(
-                    url=pr['html_url'], title=pr['title'],
-                    repo_name=pr['repository']['name'],
-                    repo_url=pr['repository']['html_url'],
-                    repo_owner=pr['repository']['owner']['login']
-                )
+    for pr in raw_pull_requests:
+        # fetch details about pull request
+        details_r = requests.get(pr['url'])
+        if details_r.status_code != 200:
+            return render_template('error.jinja2')
+        details = details_r.json()
+        # fetch details about repo
+        repo_r = requests.get(details['repository_url'])
+        if repo_r.status_code != 200:
+            return render_template('error.jinja2')
+        repo = repo_r.json()
+        pull_requests.append(
+            PullRequest(
+                url=pr['html_url'], title=pr['title'],
+                repo_name=repo['name'],
+                repo_url=repo['html_url'],
+                repo_owner=repo['owner']['login']
             )
+        )
     return render_template('progress.jinja2', pull_requests=pull_requests)
 
 if __name__ == '__main__':
