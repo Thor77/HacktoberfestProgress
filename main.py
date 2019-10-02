35578832509e7cd82fd2583a2ba72c7ec975c186
jkolthof <>
2019-10-02 09:44:43 +0200

    Added 2019 time-range / updated readme.md

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 from collections import namedtuple
 
 import requests
@@ -14,10 +15,10 @@ client_secret = app.config.get('GITHUB_CLIENT_SECRET')
 
 api_base = 'https://api.github.com'
 
-auth_url = 'https://github.com/login/oauth/authorize'\
-    '?client_id={}'.format(client_id)
+auth_url = 'https://github.com/login/oauth/authorize' \
+           '?client_id={}'.format(client_id)
 
-time_range = '2018-10-01T00:00:01Z..2018-10-31T23:59:59'
+time_range = '{year}-10-01T00:00:01Z..{year}-10-31T23:59:59'..format(year=datetime.datetime.now().year)
 search_query = 'type:pr+created:' + time_range + '+author:{}'
 
 PullRequest = namedtuple('PullRequest', [
